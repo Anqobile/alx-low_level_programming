d2866c5b62fd351584a84fba4ad443cdfc289c2e
Anqobile <anqobilegylar@gmail.com>
2023-02-21 18:06:32 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Descrtiption: Write a function that prints the alphabet, in lowercase
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= '2'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
